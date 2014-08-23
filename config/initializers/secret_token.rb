9159c315ffeded465b1126a51d1455c3a2cf8c0b
Dmitry Semenovsky <dse@Dmitrys-MacBook-Pro.local>
2014-08-23 15:46:30 +0400

    HTML markup for medium and large screens

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DigitalSunrise::Application.config.secret_key_base = '2970dc32d3dd3040349867ca6fdb5e43c20eef167458a42a2e91ecfe5b7452195e0f5b571fe6a07ccc815cc44da2aeed2e61410467e68cfe4da49cd497ce44e8'
