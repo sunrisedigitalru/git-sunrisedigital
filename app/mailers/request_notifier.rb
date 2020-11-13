4ebc9d5f00b525130c764b0caf409c75954a8bf0
Ruslan Kurmanaleev <ruslan.kurmanaleev@yandex.ru>
2020-11-13 18:34:24 +0300

    Refactor the code

--- app/mailers/request_notifier.rb
+++ app/mailers/request_notifier.rb
@@ -1,11 +1,6 @@
 class RequestNotifier < ActionMailer::Base
   default from: ENV["MAIL_FROM"]
 
-  # Subject can be set in your I18n file at config/locales/en.yml
-  # with the following lookup:
-  #
-  #   en.order_notifier.received.subject
-  #
   def received(message)
     @message = message
     mail to: ENV["MAIL_TO"]
