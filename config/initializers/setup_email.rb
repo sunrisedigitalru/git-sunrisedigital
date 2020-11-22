de7ac153574b56a4a7562675b9dee4befcc87940
Ruslan Kurmanaleev <ruslan.kurmanaleev@yandex.ru>
2020-11-22 17:22:58 +0300

    Update Mail.ru SMTP Settings

--- config/initializers/setup_email.rb
+++ config/initializers/setup_email.rb
@@ -1,8 +1,10 @@
 ActionMailer::Base.smtp_settings = {
-  :address              => "smtp.yandex.ru",                     
-  :port                 => 587,
-  :user_name            => ENV["MAIL_USERNAME"],
-  :password             => ENV["MAIL_PASS"],
-  :authentication       => "login",
-  :enable_starttls_auto => true
+  address: "smtp.mail.ru",                     
+  port: 465,
+  domain: "mail.ru",
+  user_name: ENV["MAIL_USERNAME"],
+  password: ENV["MAIL_PASS"],
+  authentication: "plain",
+  enable_starttls_auto: true,
+  tls: true
 }
